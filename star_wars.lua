19cb828f9c02aa4720a638dd13323ddb517f8686
Aidan <aidangreen99@gmail.com>
2021-07-05 22:41:34 -0700

    Duh

--- star_wars.lua
+++ star_wars.lua
@@ -4,29 +4,6 @@
 -- For SMW, make sure you have a save state named "DP1.state" at the beginning of a level,
 -- and put a copy in both the Lua folder and the root directory of BizHawk.
 
-if gameinfo.getromname() == "Super Mario World (USA)" then
-	Filename = "DP1.state"
-	ButtonNames = {
-		"A",
-		"B",
-		"X",
-		"Y",
-		"Up",
-		"Down",
-		"Left",
-		"Right",
-	}
-elseif gameinfo.getromname() == "Super Mario Bros." then
-	Filename = "SMB1-1.state"
-	ButtonNames = {
-		"A",
-		"B",
-		"Up",
-		"Down",
-		"Left",
-		"Right",
-	}
-end
 Filename = "StarWarsLevel1.state"
 ButtonNames = {
 	"A",
